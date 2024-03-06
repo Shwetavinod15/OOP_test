60ec5f2a5e398cedc9e1ab3711b7c4e6faee757b
Sameer Krishna Enjapuri <senjapuri@usf.edu>
2024-03-06 13:59:28 -0500

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_test
+{
+    internal class Person
+    {
+        public static string SalutationMr = "Mr.";
+        public static string SalutationMs = "Ms.";
+
+        public string FirstName;
+        public string LastName;
+        public string salutation;
+        public Person()
+        {
+            FirstName = "Sameer";
+            LastName = "Enjapuri";
+            salutation = SalutationMr;
+        }
+        public Person(string firstName, string lastName, string salutation)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            this.salutation = SalutationMr;
+        }
+        public string getName()
+        {
+            return FirstName + ' ' + LastName;
+        }
+    }
+}
